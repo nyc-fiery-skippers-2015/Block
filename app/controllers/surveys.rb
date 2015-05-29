de5a883cdb623958871d272439abd6c7bc16293a
Antonio <manentea@gmail.com>
2015-05-29 16:25:09 -0400

    added small validation and survey views

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -3,26 +3,23 @@ get '/surveys' do
   erb :'surveys/index', locals: {surveys: all_surveys}
 end
 
+get '/surveys/new' do
+  erb :'/surveys/new'
+end
+
 get '/surveys/:id' do
   cur_survey = Survey.find_by(id: params[:id])
+  if !cur_survey
+    cur_survey = Survey.find_by(url: params[:id])
+  end
   return [500, "No Survey Found"] unless cur_survey
   erb :'surveys/show', locals: {survey: cur_survey}
-end
-
-get '/surveys/:url' do
-  cur_survey = Survey.find_by(url: params[:url])
-  return [500, "No Survey Found"] unless cur_survey
-  erb :'surveys/show', locals: {survey: cur_survey}
-end
-
-get '/surveys/new' do
-  # erb: '/surveys/new'
 end
 
 post '/surveys' do
   user_input = params[:survey]
   rand_url = SecureRandom.hex(4)
-  new_survey = Survey.new(name: user_input[:name], creator_id: session[:user_id], url: rand_url)
+  new_survey = Survey.new(name: user_input[:name], creator_id: 1, url: rand_url)
   return [500, "Survey Must Have a Name"] unless new_survey.save
   redirect '/surveys'
 end
