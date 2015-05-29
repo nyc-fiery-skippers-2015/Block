abf5264c58d56e0c6e80e5c3e16e6a27df34d182
Antonio <manentea@gmail.com>
2015-05-29 18:09:06 -0400

    adding question with ajax

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,12 +1,12 @@
 get '/surveys/:id/questions/new' do
   cur_survey = {id: params[:id]}
-  erb :'/questions/new', locals: {survey: cur_survey}
+  erb :'/questions/new', locals: {survey: cur_survey}, layout: false
 end
 
 post '/surveys/:id/questions' do
   new_question = Question.new(body: params[:question][:body], survey_id: params[:id])
   return [500, "Invalid Question"] unless new_question.save
-  redirect "/surveys/#{new_question.survey_id}"
+  erb :'/questions/show', locals: {question: new_question}, layout: false
 end
 
 delete 'surveys/:id/questions/:question_id' do
