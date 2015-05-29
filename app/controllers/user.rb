1b52505e1e2a11aad8531feca8cc5f3545277f32
Marc Cardacci <marc.cardacci@gmail.com>
2015-05-29 19:40:26 -0400

    adds user controller and views

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,22 +1,34 @@
-#profile page
+get '/users' do
+  require_logged_in
+  all_users = User.all
+  erb :'user/show_all', locals: { users: all_users}
+end
+
+get '/user/areyousure' do
+  erb :'user/areyousure'
+end
+
 get '/user/:id' do
   current_user = User.find_by(id: params[:id])
-  erb :'index'
+  erb :'user/index', locals: { user: current_user }
 end
 
-get '/users' do
-  all_user = User.all
-  erb :'user/show_all'
-end
 
 get '/user/:id/edit' do
+  require_logged_in
   erb :'user/edit'
 end
 
+
 put '/user/edit' do
+  cur_user = User.find_by(id: session[:user_id])
+  cur_user.update(params[:user])
+  return [500, "Invalid Entry"] unless cur_user.save
   redirect "/user/#{current_user.id}"
 end
 
-delete '/user/:id' do
+delete '/user/delete' do
+  current_user = User.find_by(id: session[:user_id])
+  current_user.destroy
   redirect '/'
 end
