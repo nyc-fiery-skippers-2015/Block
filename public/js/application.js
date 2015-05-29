f1b6dd170841f5200ba087ff12c84530b3a1050d
Antonio <manentea@gmail.com>
2015-05-29 18:09:06 -0400

    adding question with ajax

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,42 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.new_question').on('click', questionForm);
+  $('.survey-show').on('submit','.new_question_form', addQuestion);
 });
+
+var questionForm = function(event) {
+  event.preventDefault();
+  var $target = $(event.target);
+  var controllerMethod = 'get';
+  var surveyId = $target.attr('id');
+  var controllerRoute = '/surveys/' + surveyId + '/questions/new'
+
+  $.ajax({
+    'url': controllerRoute,
+    'method': controllerMethod,
+    'datatype': 'html'
+  }).done(function(response) {
+    $('.survey-show').append(response)
+  }).fail(function(error){
+    console.log(error)
+  });
+};
+
+var addQuestion = function(event){
+  event.preventDefault();
+  var $target = $(event.target);
+  var controllerMethod = 'post';
+  var controllerRoute = $target.attr('action');
+  var myData = $target.serialize();
+
+  $.ajax({
+    'url': controllerRoute,
+    'method': controllerMethod,
+    'data': myData,
+    'datatype': 'html'
+  }).done(function(response){
+    $('.new_question_form').toggle(false)
+    $('.survey-questions').append(response)
+  }).fail(function(error){
+    console.log(error)
+  });
+};
