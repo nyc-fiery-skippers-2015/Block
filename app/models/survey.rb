16e9f27aee41809b8ba4d2394a2c5280049b86c3
Antonio <manentea@gmail.com>
2015-05-29 12:35:32 -0400

    models and migrations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,7 @@
 class Survey < ActiveRecord::Base
-  # Remember to create a migration!
+
+  belongs_to :creator, class_name: "User"
+  has_many :questions
+  has_many :survey_answers
+
 end
--- db/migrate/20150529115002_create_survey_answers.rb
+++ db/migrate/20150529115002_create_survey_answers.rb
@@ -1,4 +1,10 @@
 class CreateSurveyAnswers < ActiveRecord::Migration
   def change
+    create_table :survey_answers do |t|
+      t.integer :user_id, null: false
+      t.integer :survey_id, null: false
+      t.integer :answer_id, null: false
+      t.timestamps null: false
+    end
   end
 end
