395e76a7aa076159b588d948dc69ae698b176dc4
dholness2 <DHolness@Dons-MacBook-Pro.local>
2015-05-30 23:36:42 -0400

    updated the sed file and stats controlers and views

--- app/controllers/stats.rb
+++ app/controllers/stats.rb
@@ -2,24 +2,25 @@ require 'pry'
 get '/stats/:id' do
  current_survey = Survey.find_by(id:params[:id])
  questions = current_survey.questions
- submits = (SurveyAnswer.all().count)/(questions.count)
-  p stats = question_stats(questions)
+ submits = (current_survey.survey_answers.count)/(3 )
+  stats = question_stats(questions)
   #numer of survey responders-answers/question= survey user resposes
- erb :'stats/stats', locals:{survey: current_survey,questions:questions,submits:submits, stats:questions_stats }
+ erb :'stats/stats', locals:{survey: current_survey,questions:questions,submits:submits, stats:stats }
 end
 
 def find_count(id )
-binding.pry
-  ans = SurveyAnswer.find_by(answer_id:id)
+
+  ans = SurveyAnswer.count(answer_id:id)
 
 end
 
-def ans_stats (ques)
- answers = ques.answers
- a = find_count((answers[0].id))
- b = find_count((answers[1].id))
- c = find_count((answers[2].id))
- d = find_count((answers[3].id))
+def ans_stats (question)
+
+ answers = question.answers
+ a = (answers[0].answer) + ((find_count((answers[0].id))).to_s )
+ b = (answers[1].answer) + ((find_count((answers[1].id))).to_s )
+ c = (answers[2].answer) +((find_count((answers[2].id))).to_s )
+  d = (answers[3].answer) +((find_count((answers[3].id))).to_s )
  ans_stats = [a,b,c,d]
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,24 @@
-User.create(name:"Don Holness", email:"dhh@gmail.com", password:"dh")
-User.create(name:"bh", email:"bh@gmail.com", password:"bh")
+User.create(name:"Don Holness", email:"dhh@gmail.com", password:"1234567")
+User.create(name:"bhh", email:"bh@gmail.com", password:"1234567")
 Survey.create(name:"Test_survey",creator_id:1,url:"test1" )
+
 Question.create(body:"who am I?",survey_id:1 )
 Answer.create(answer:"Don",question_id:1)
-SurveyAnswer.create(user_id:2,survey_id:1,answer_id:1)
+Answer.create(answer:"testans2",question_id:1)
+Answer.create(answer:"testans2",question_id:1)
+Answer.create(answer:"testans3",question_id:1)
+
 Question.create(body:"where am I?",survey_id:1 )
-Answer.create(answer:"NY",question_id:1)
-SurveyAnswer.create(user_id:2,survey_id:1,answer_id:2)
+Answer.create(answer:"NY",question_id:2)
+Answer.create(answer:"testans2",question_id:2)
+Answer.create(answer:"testans2",question_id:2)
+Answer.create(answer:"testans3",question_id:2)
+
 Question.create(body:"what time is it?",survey_id:1 )
-Answer.create(answer:"5pm",question_id:1)
-SurveyAnswer.create(user_id:2,survey_id:1,answer_id:3)
+Answer.create(answer:"5pm",question_id:3)
+Answer.create(answer:"testans2",question_id:3)
+Answer.create(answer:"testans2",question_id:3)
+Answer.create(answer:"testans3",question_id:3)
+
+
+20.times{|x| SurveyAnswer.create(user_id:x,survey_id:1,answer_id:(rand(1..4)))}
